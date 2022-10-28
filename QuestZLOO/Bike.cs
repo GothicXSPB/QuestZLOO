804cca6778d60c6d70f9940ec8edc0511799bde8
GothicXSPB <ZZZerggg@mail.ru>
2022-10-28 12:05:04 +0300

    Add project files.

---
+++ QuestZLOO/Bike.cs
@@ -0,0 +1,22 @@
+﻿namespace QuestZLOO
+{
+    public class Bike
+    {
+        public string Label { get; private set; }
+
+        public string Color { get; set; }
+
+        public int Age { get; private set; }
+
+        public string Material { get; private set; }
+
+        public int Seater { get; set; }
+
+        public int Wheel { get; set; }
+
+        public Bike(string label, string color,string material)
+        { 
+        
+        }         
+    }
+}
---
+++ QuestZLOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text.Json;
+
+
+namespace QuestZLOO
+{
+    public class Program
+    {
+        static async void Main(string[] args)
+        {
+            string path = "Bike.txt";
+
+            Bike bike = new Bike("stels", "black", "steel");
+
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                await writer.WriteLineAsync(Bike);
+            }
+        }
+
+    }
+}
